fe5fc40876abe1a9d265e9c3e7e381fd97f167af
Ashwin <aswinjeganath@gmail.com>
2018-10-21 02:01:24 +0530

    Add interactivity to navbar and add transition styles

--- js/script.js
+++ js/script.js
@@ -2,10 +2,21 @@ const hamburger = document.querySelector('#hamburger');
 const navbar = document.querySelector('#nav');
 const closeBtn = document.querySelector('#nav-close');
 
-hamburger.addEventListener('click', function() {
+function closeNav() {
+  navbar.classList.remove('nav--open');
+  document.body.classList.remove('nav-menu-open');
+}
+
+hamburger.addEventListener('click', function(event) {
+  event.stopPropagation();
   navbar.classList.add('nav--open');
+  document.body.classList.add('nav-menu-open');
 });
 
-closeBtn.addEventListener('click', function() {
-  navbar.classList.remove('nav--open');
+closeBtn.addEventListener('click', closeNav);
+
+document.body.addEventListener('click', closeNav);
+
+navbar.addEventListener('click', function(event) {
+  event.stopPropagation();
 });
